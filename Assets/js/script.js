207d3f6841f6c211f9f63a92d7e21dc3956c7c71
Keerbi Liu <kirby.liu12@gmail.com>
2021-03-18 18:50:42 +1100

    adding password generator function.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,23 +3,38 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //Added a prompt for password length.
+  //Added prompts for password criteria.
   var passwordLength=prompt("Password length (8-128)");
-  var lowerCase=prompt("Include lowercase characters? (Y or N)");
-  var upperCase=prompt("Include uppercase characters? (Y or N)");
-  var includeNumbers=prompt("Include numbers? (Y or N)");
-  var includeSymbols=prompt("Include symbols? (Y or N)");
+  var lower=prompt("Include lowercase characters? (Y or N)");
+  var upper=prompt("Include uppercase characters? (Y or N)");
+  var number=prompt("Include numbers? (Y or N)");
+  var symbol=prompt("Include symbols? (Y or N)");
 
-  var password = generatePassword(passwordLength, lowerCase, upperCase, includeNumbers, includeSymbols);
+  hasLower=lower;
+  hasUpper=upper;
+  hasNumber=number;
+  hasSymbol=symbol;
+  length=passwordLength;
+
+  var password = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
  
+//generatePassword function
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  var typesCount = lower + upper + number + symbol;
+  console.log("types count:", typesCount);
+  var typesArr = [{lower}, {upper}, {number}, {symbol}];
+  console.log("types Arr:", typesArr);
+   
+}
 
 // Random functions for lowercase, uppercase, number and symbols.
 function getRandomLower() {
