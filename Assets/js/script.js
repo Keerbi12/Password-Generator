38c029f5ca72682c7ca504c5d9055d03df5cb931
Keerbi Liu <kirby.liu12@gmail.com>
2021-03-18 17:13:35 +1100

    Added prompts for password generator.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,11 +4,13 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   //Added a prompt for password length.
-  var charLength=prompt("Password length (8-128)");
-  alert("The password length has been set to " + charLength + ".");
-  var special=prompt("Include special characters")
+  var passwordLength=prompt("Password length (8-128)");
+  var lowerCase=prompt("Include lowercase characters? (Y or N)");
+  var upperCase=prompt("Include uppercase characters? (Y or N)");
+  var includeNumbers=prompt("Include numbers? (Y or N)");
+  var includeSymbols=prompt("Include symbols? (Y or N)");
 
-  var password = generatePassword(charLength, includeUppercase, includeNumbers, includeSymbols);
+  var password = generatePassword(passwordLength, lowerCase, upperCase, includeNumbers, includeSymbols);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -19,7 +21,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
  
 
-
+// Random functions for lowercase, uppercase, number and symbols.
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -32,7 +34,13 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*()-_=+[]{}/\|;:,.?";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 
 console.log(getRandomLower());
 console.log(getRandomUpper());
 console.log(getRandomNumber());
+console.log(getRandomSymbol());
