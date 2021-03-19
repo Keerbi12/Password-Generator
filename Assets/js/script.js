784a9ef27a35f8b738c8b0c235b8b74284835764
Keerbi Liu <kirby.liu12@gmail.com>
2021-03-19 21:45:29 +1100

    Edited read me file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,15 +13,15 @@ var upper;
 var number;
 var symbol;
 
-// Write password to the #password input
+//Write password to the #password input in the HTML.
 function writePassword(lower, upper, number, symbol, length) {
-//Added prompts for password criteria.
+//Added prompts for password criteria. + makes all inputs numbers rather than string.
   length=+prompt("Password length (8-128)");
   lower=+prompt("Include lowercase characters? (1 = YES or 0 = NO)");
   upper=+prompt("Include uppercase characters? (1 = YES or 0 = NO)");
   number=+prompt("Include numbers? (1 = YES or 0 = NO)");
   symbol=+prompt("Include symbols? (1 = YES or 0 = NO)");
-
+//Just here for checking purposes, length needs to be number not a string.
   console.log(lower, upper, number, symbol);
   console.log(typeof length)
   
@@ -34,21 +34,22 @@ function writePassword(lower, upper, number, symbol, length) {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
  
-//writing the generatePassword function
+//writing the generatePassword function.
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = "";
   var typesCount = lower + upper + number + symbol;
+//To make sure the the types count is correct. 
   console.log("types count:", typesCount);
   var typesArr = [{lower}, {upper}, {number}, {symbol}].filter
   (
     item => Object.values(item)[0]
   );
   console.log("types Arr:", typesArr);
-
+//If no input is put into prompts, return "No input added".
   if(typesCount === 0) {
     return "No input added";
   }
-
+//Making the length of the password between 8-128.
   if(length < 8) {
     return "Password needs to be longer than 8 characters.";
   }
@@ -56,7 +57,7 @@ function generatePassword(lower, upper, number, symbol, length) {
   if(length > 128) {
     return "Password needs to be shorther than 128 characters."
   }
-
+//This will loop for i < length, if all 4 criterias are ticked, this will tick up by 4, lowercase, uppercase, number, symbol.
   for(let i = 0; i < length; i += typesCount) {
     typesArr.forEach(type => {
       var functionName = Object.keys(type)[0];
@@ -65,8 +66,10 @@ function generatePassword(lower, upper, number, symbol, length) {
       generatedPassword += randomFunction[functionName]();
     });
   }
+//Since the password generates is a multiple of 4 (if 4 criterias are selected), we need to use .slice to cut it to the password length. 
   var finalPassword = generatedPassword.slice(0, length);
   console.log(length)
+//returns finalPassword to the box. 
   return finalPassword;
 }
 
@@ -75,6 +78,9 @@ function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+
+//math.random will give a decimal between 1 and 0, if multiplied by 26, it give a decimal between 0 and 26, math.floor will round down to the nearest integer.
+//26 is used as there are 26 letters in the alphabet, we have to add 65, because the uppercase letters start from 65 and there are 26 of them.
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
@@ -84,6 +90,6 @@ function getRandomNumber() {
 }
 
 function getRandomSymbol() {
-  var symbols = "!@#$%^&*()-_=+[]{}/\|;:,.?";
+  var symbols = "!@#$%^&*()-_=+[]{}/;:,.?";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Password-Generator
 Random password generator
+
+I originally wanted to create check boxes and number sliders to set the criteria of the password, but the homework guide requires prompts to be used.
+
+When the website is loaded up, you can click generate password, it will then open a series of prompts on the page.
+
+type a number between 8 and 128 for password length.
+
+type either 1 for yes to lowercase letters or 0 for no to lowercase letters.
+
+type either 1 for yes to uppercase letters or 0 for no to uppercase letters.
+
+type either 1 for yes to numbers or 0 for no to numbers.
+
+type either 1 for yes to symbols or 0 for no to symbols.
+
+password will then be generated in the text box.
+
+This exercise teaches the importance of Java Script and how it can be used to create complex engines such as a random password generator. 
+this homework provides practise on how to use arrays, functions and logic to create these systems.
